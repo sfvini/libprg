a72ab60d75cd5425506468ddc46733e8080be71f
sfvini <vinicius.sf2007@aluno.ifsc.edu.br>
2025-09-25 11:04:26 -0300

    Inicio lista encadeada

--- libprg/src/include/libprg/libprg.h
+++ libprg/src/include/libprg/libprg.h
@@ -35,7 +35,10 @@ void inserir_lista(lista_t *l, int valor);
 void apagar_lista(lista_t *l);
 void excluir_item_lista(lista_t *l, int valor);
 void listar_lista(lista_t *l);
-bool lista_linear(lista_t *l);
-bool lista_vazia(lista_t *l);
+
+//LISTA ENCADEADA
+typedef struct no no_t;
+no_t* criar_lista_encadeada(int valor);
+no_t* adicionar_lista_encadeada(no_t* inicio, int valor);
 
 #endif
---
+++ libprg/src/libprg/lista_encadeada.c
@@ -0,0 +1,26 @@
+#include "libprg/libprg.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct no {
+    int valor;
+    struct no_t* proximo;
+} no_t;
+
+no_t* criar_lista_encadeada(int valor) {
+    no_t* no = malloc(sizeof(no_t));
+    no->valor = valor;
+    no->proximo = NULL;
+
+    return no;
+}
+
+no_t* adicionar_lista_encadeada(no_t* inicio, int valor) {
+
+    no_t* novo_no = criar_lista_encadeada(valor);
+    novo_no->proximo = inicio;
+    inicio = novo_no;
+
+    return inicio;
+}
+
