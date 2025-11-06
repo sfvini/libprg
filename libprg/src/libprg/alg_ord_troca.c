be3b6658e9bd3e07c64211e97ad27a06f827c799
Vini <viniciusicmsc@gmail.com>
2025-11-06 16:43:24 -0300

    insertion sort

--- libprg/src/libprg/alg_ord_troca.c
+++ libprg/src/libprg/alg_ord_troca.c
@@ -9,3 +9,15 @@ void bubble_sort(int* arr, int tamanho) {
         }
     }
 }
+
+void insertion_sort(int* arr, int tamanho) {
+    for (int i = 1; i < tamanho; i++) {
+        int key = arr[i];
+        int j = i - 1;
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j -= 1;
+        }
+        arr[j + 1] = key;
+    }
+}
